0fb0f39f61722da845561fccd9e773a19ddfa70f
Taowei Huang <taoweihuang914@gmail.com>
2025-06-14 17:04:09 -0500

    working on kde

--- kde_test.py
+++ kde_test.py
@@ -3,20 +3,37 @@ import torch
 import matplotlib.pyplot as plt
 import numpy as np
 
-multivariate_normal = torch.distributions.MultivariateNormal(torch.zeros(2), torch.eye(2))
+multivariate_normal = torch.distributions.MultivariateNormal(torch.zeros(1), torch.eye(1))
 X = multivariate_normal.sample((50000,)) # create data
 plt.figure()
-plt.hist2d(X[:,0],X[:,1], bins=100)
-plt.colorbar()
+if X.shape[1] == 1:
+    plt.hist(X, bins=30)
+elif X.shape[1] == 2:
+    plt.hist2d(X[:,0],X[:,1], bins=100)
+    plt.colorbar()
+else:
+    pass
 plt.title("from normal")
 plt.show()
 
-kde = KernelDensity(bandwidth=1.0, kernel='gaussian') # create kde object with isotropic bandwidth matrix
+kde = KernelDensity(bandwidth=0.1, kernel='gaussian') # create kde object with isotropic bandwidth matrix
 _ = kde.fit(X) # fit kde to data
 
-X_new = kde.sample(50000) # sample from estimated density
+# X_new = kde.sample(5) # sample from estimated density
+X_new = torch.distributions.Uniform(-4,4).sample((10000,))
+score = kde.score_samples(X_new.unsqueeze(1))
+n_score = torch.exp(score)
+X_accepted = X_new[n_score > 3e-1]
+X_new = X_accepted.unsqueeze(1)
+
+
 plt.figure() 
-plt.hist2d(X_new[:,0],X_new[:,1], bins=100)
-plt.colorbar()
+if X_new.shape[1] == 1:
+    plt.hist(X_new, bins=30)
+elif X_new.shape[1] == 2:    
+    plt.hist2d(X_new[:,0],X_new[:,1], bins=100)
+    plt.colorbar()
+else:
+    pass
 plt.title("from kde")
 plt.show()

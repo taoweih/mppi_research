2b40c7d710e70b1976923cce376079f288f245d2
Taowei Huang <taoweihuang914@gmail.com>
2025-08-05 11:23:57 -0500

    modify ant task

---
+++ hydrax_files/hydrax/hydrax/tasks/ant.py
@@ -0,0 +1,61 @@
+from typing import Dict
+
+import jax
+import jax.numpy as jnp
+import mujoco
+from mujoco import mjx
+
+from hydrax import ROOT
+from hydrax.task_base import Task
+
+
+class Ant(Task):
+    """Standup task for the Unitree G1 humanoid."""
+
+    def __init__(self) -> None:
+        """Load the MuJoCo model and set task parameters."""
+        mj_model = mujoco.MjModel.from_xml_path(ROOT + "/models/ant/scene.xml")
+        super().__init__(
+            mj_model,
+            trace_sites= None #["imu_in_torso", "left_foot", "right_foot"],
+        )
+
+        self.end_effector_pos_id = mujoco.mj_name2id(
+            self.mj_model, mujoco.mjtObj.mjOBJ_BODY, "torso_site"
+        )
+        self.goal_pos_id = mujoco.mj_name2id(
+            self.mj_model, mujoco.mjtObj.mjOBJ_BODY, "goal"
+        )
+
+    def running_cost(self, state: mjx.Data, control: jax.Array) -> jax.Array:
+        end_effector_pos = state.xpos[self.end_effector_pos_id]
+        goal_pos = state.xpos[self.goal_pos_id]
+
+        cost = 10*jnp.sum(jnp.square(end_effector_pos - goal_pos),axis=0)
+        return cost
+
+    def terminal_cost(self, state: mjx.Data) -> jax.Array:
+        """The terminal cost ϕ(x_T)."""
+        return self.running_cost(state, jnp.zeros(self.model.nu))
+
+    def domain_randomize_model(self, rng: jax.Array) -> Dict[str, jax.Array]:
+        """Randomize the friction parameters."""
+        n_geoms = self.model.geom_friction.shape[0]
+        multiplier = jax.random.uniform(rng, (n_geoms,), minval=0.5, maxval=2.0)
+        new_frictions = self.model.geom_friction.at[:, 0].set(
+            self.model.geom_friction[:, 0] * multiplier
+        )
+        return {"geom_friction": new_frictions}
+
+    def domain_randomize_data(
+        self, data: mjx.Data, rng: jax.Array
+    ) -> Dict[str, jax.Array]:
+        """Randomly perturb the measured base position and velocities."""
+        rng, q_rng, v_rng = jax.random.split(rng, 3)
+        q_err = 0.01 * jax.random.normal(q_rng, (7,))
+        v_err = 0.01 * jax.random.normal(v_rng, (6,))
+
+        qpos = data.qpos.at[0:7].set(data.qpos[0:7] + q_err)
+        qvel = data.qvel.at[0:6].set(data.qvel[0:6] + v_err)
+
+        return {"qpos": qpos, "qvel": qvel}
